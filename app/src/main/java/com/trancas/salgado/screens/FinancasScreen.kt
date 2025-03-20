29b4e80e23adbfff52aa16047c8ed0dc1ebc6649
DanielRRSilva <daniel.rsilva@sptech.school>
2025-03-20 14:14:09 -0300

    fix - fundo da tela de financas

--- app/src/main/java/com/trancas/salgado/screens/FinancasScreen.kt
+++ app/src/main/java/com/trancas/salgado/screens/FinancasScreen.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -37,90 +39,88 @@ import com.trancas.salgado.ui.theme.flame_pea
 
 @Composable
 fun FinancasScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.testefundo),
-            contentDescription = "imagem de fundo",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
+    BoxWithConstraints {
+        val screenHeight = maxHeight
+        Box(
+            Modifier.background(flame_pea)
+                .fillMaxWidth()
+                .height(screenHeight * 0.24f)
         )
-        Column(
-            modifier = Modifier.fillMaxSize()
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            modifier = Modifier.padding(top = 70.dp, start = 18.dp),
+            text = "Olá, Paula!",
+            fontSize = 28.sp,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            Text(
-                modifier = Modifier.padding(top = 70.dp, start = 18.dp),
-                text = "Olá, Paula!",
-                fontSize = 28.sp,
-                color = Color.White
-            )
-            Spacer(modifier = Modifier.height(50.dp))
-            Column (
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(10.dp)
+            Box(
+                modifier =  Modifier
+                    .width(320.dp)
+                    .height(170.dp)
+                    .shadow(8.dp, shape = RoundedCornerShape(24.dp))
+                    .clip(shape = RoundedCornerShape(24.dp))
+                    .background(Color.White)
             ) {
-                Box(
-                    modifier =  Modifier
-                        .width(320.dp)
-                        .height(170.dp)
-                        .shadow(8.dp, shape = RoundedCornerShape(24.dp))
-                        .clip(shape = RoundedCornerShape(24.dp))
-                        .background(Color.White)
+                Column (
+                    modifier = Modifier.fillMaxHeight(),
                 ) {
-                    Column (
-                        modifier = Modifier.fillMaxHeight(),
-                    ) {
+                    Text(
+                        modifier = Modifier.padding(top = 25.dp, start = 25.dp, bottom = 5.dp),
+                        text = "Lucro Fevereiro",
+                        fontSize = 21.sp,
+                        fontWeight = FontWeight.Medium,
+                    )
+                    Divider(color = Color.LightGray, thickness = 1.dp, modifier = Modifier.padding(vertical = 10.dp))
+                    Row {
                         Text(
-                            modifier = Modifier.padding(top = 25.dp, start = 25.dp, bottom = 5.dp),
-                            text = "Lucro Fevereiro",
-                            fontSize = 21.sp,
-                            fontWeight = FontWeight.Medium,
+                            modifier = Modifier.padding(top = 28.dp, start = 25.dp),
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold,
+                            text = "R$ 1.000,00",
                         )
-                        Divider(color = Color.LightGray, thickness = 1.dp, modifier = Modifier.padding(vertical = 10.dp))
-                        Row {
-                            Text(
-                                modifier = Modifier.padding(top = 28.dp, start = 25.dp),
-                                fontSize = 22.sp,
-                                fontWeight = FontWeight.Bold,
-                                text = "R$ 1.000,00",
-                            )
-                        }
                     }
                 }
-                Spacer(Modifier.height(20.dp))
-                Button(
-                    modifier =  Modifier
-                        .width(320.dp)
-                        .height(90.dp),
-                    shape = RoundedCornerShape(16.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = flame_pea),
-                    onClick = { }
-                ) {
-                  Column(
+            }
+            Spacer(Modifier.height(20.dp))
+            Button(
+                modifier =  Modifier
+                    .width(320.dp)
+                    .height(90.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = flame_pea),
+                onClick = { }
+            ) {
+                Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center
-                  ) {
-                      Icon(
-                          painter = painterResource(id = R.drawable.file_icon),
-                          contentDescription = "Seta para a direita",
-                          modifier = Modifier.size(40.dp)
-                      )
-                      Spacer(Modifier.height(5.dp))
-                      Text(
-                          text = "Extrato",
-                          fontSize = 18.sp,
-                          fontWeight = FontWeight.SemiBold,
-                      )
-                  }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.file_icon),
+                        contentDescription = "Seta para a direita",
+                        modifier = Modifier.size(40.dp)
+                    )
+                    Spacer(Modifier.height(5.dp))
+                    Text(
+                        text = "Extrato",
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                    )
                 }
             }
         }
     }
 }
+
 
 
 @Preview(
