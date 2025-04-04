06e41c85bfc9f5f3418de1911a3e64f50f542cd5
jean-rocha-santos <jean.santos@sptech.school>
2025-04-03 21:50:36 -0300

    Adicionando nova activity para tela inicial

---
+++ app/src/main/java/com/trancas/salgado/ui/InitialActivity.kt
@@ -0,0 +1,120 @@
+package com.trancas.salgado.ui
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.trancas.salgado.R
+import com.trancas.salgado.ui.theme.AppTheme
+
+class InitialActivity : ComponentActivity() {
+    class Navegacao : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "TelaInicial") {
+                    composable("Login") { InitialScreen(navController) }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun InitialScreen(navController: NavController) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+            .background(Color(0xFF3F522D))
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_inicio),
+            contentDescription = "Paula Salgado",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(405.dp)
+        )
+        Box(
+            modifier = Modifier
+
+                .height(350.dp)
+                .width(400.dp)
+
+                .padding(top = 5.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .fillMaxHeight()
+
+
+            ) {
+
+                Text(
+                    text = "SALGADO\n\nTRANÇAS",
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Button(
+                    onClick = { navController.navigate("Login") },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFB55B49)),
+                    shape = RoundedCornerShape(50.dp),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .size(width = 220.dp, height = 50.dp)
+                ) {
+                    Text(text = "Entrar", color = Color.White, fontSize = 18.sp)
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    AppTheme {
+        InitialScreen(navController = rememberNavController())
+    }
+}
