f582ca7709de95aafdf701b162b578d3880c77b5
jean-rocha-santos <jean.santos@sptech.school>
2025-04-13 21:38:23 -0300

    view model addClient

--- app/src/main/java/com/trancas/salgado/screens/client/ClientViewModel.kt
+++ app/src/main/java/com/trancas/salgado/screens/client/ClientViewModel.kt
@@ -1,24 +1,49 @@
 package com.trancas.salgado.screens.client
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.trancas.salgado.service.SalgadoApi
+import kotlinx.coroutines.launch
 
 class ClientViewModel : ViewModel() {
 
-    //private val api =SalgadoApi.api
+    private val api = SalgadoApi.api
     private var nome =""
     private var email =""
     private var telefone =""
     private val _erros= mutableStateListOf<String>();
 
     fun addClient(nome: String, email: String, telefone: String) {
+        _erros.clear();
+        viewModelScope.launch {
+            try {
 
-        try{
+                if (nome.isEmpty()) {
+                    _erros.add("Nome não pode ser vazio")
+                }
+                if (email.isEmpty()) {
+                    _erros.add("Email não pode ser vazio")
+                }
+                if (telefone.isEmpty()) {
+                    _erros.add("Telefone não pode ser vazio")
+                }
+                if (_erros.isEmpty()) {
 
-        }catch (ex :Exception){
-            _erros.add("Erro ao adicionar cliente")
+                    val client = AddClientData(nome, email, telefone);
+                    val response = api.postClient(client);
+                    Log.d("API", "Cliente adicionado com sucesso")
+                } else {
+                    Log.d("API", "Erro: ${_erros.joinToString(", ")}")
+                }
+            } catch (e: Exception) {
+                Log.d("API", "Erro: ${e.message}")
+                _erros.add("Erro ao adicionar cliente")
+                //corrigir Log.d para Log.e
+            }
         }
     }
 }
--- app/src/main/java/com/trancas/salgado/service/ClientService.kt
+++ app/src/main/java/com/trancas/salgado/service/ClientService.kt
@@ -1,5 +1,37 @@
 package com.trancas.salgado.service
 
-interface ClientService{
+import com.trancas.salgado.screens.client.AddClientData
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.http.Body
+import retrofit2.http.POST
 
+
+interface ClientService {
+    @POST("cadastros/cliente")
+    suspend fun postClient(
+        @Body client: AddClientData
+    )
 }
+object SalgadoApi {
+    private val BASE_URL = "https://10.0.0.2:8080/";
+
+    val api: ClientService by lazy {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .build()
+            .create(ClientService::class.java)
+    }
+}
+
+
