660bc80acdd39fd2f1c6fdcc3f26dbc553412de1
jean-rocha-santos <jean.santos@sptech.school>
2025-05-04 13:39:42 -0300

    adicionando SalgadoApi no Koin

--- app/src/main/java/com/trancas/salgado/AppKoin.kt
+++ app/src/main/java/com/trancas/salgado/AppKoin.kt
@@ -1,8 +1,13 @@
 package com.trancas.salgado
 
 import android.app.Application
+import com.trancas.salgado.service.LoginService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class AppKoin : Application() {
     override fun onCreate() {
@@ -14,5 +19,24 @@ class AppKoin : Application() {
 
             modules(appModule)
         }
+    }
+}
+object SalgadoApi {
+    private val BASE_URL = "http://10.0.2.2:8080/api/";
+
+    fun getApi(token: String): LoginService {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(LoginService::class.java)
     }
 }
--- app/src/main/java/com/trancas/salgado/service/LoginService.kt
+++ app/src/main/java/com/trancas/salgado/service/LoginService.kt
@@ -18,22 +18,3 @@ interface LoginService {
     ): LoginResponseData
 }
 
-object SalgadoApi {
-    private val BASE_URL = "http://10.0.2.2:8080/api/";
-
-    fun getApi(token: String): LoginService {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(interceptor)
-            .build()
-
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-            .create(LoginService::class.java)
-    }
-}
